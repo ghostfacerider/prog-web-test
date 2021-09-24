6af7c5dbfbe6ed59bb9e2169bf22fee91bd8dd3d
gb <w0202057@campus.nscc.ca>
2021-09-24 09:46:24 -0300

    added more basid javeScrip

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+//user input
+
+// use an external library to get sychronous input from the user
+var prompt = require("prompt-sync")()
+
+//prompt user for input
+var name = prompt("enter your name: ")
+
+// user the inputted value
+console.log(`hi, ${name}!`)
